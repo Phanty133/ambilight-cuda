cc8c5de1dda129477fc86d324fb741335ece5790
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2023-01-16 22:18:09 +0100

    Initialized C# Avalonia front-end

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace YetAnotherAmbilightKnockoff;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
